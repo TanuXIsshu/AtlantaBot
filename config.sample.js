c2c8f837e593c1379c5bc53da2f416659f8dbcfa
TanuXIsshu <93700134+TanuXIsshu@users.noreply.github.com>
2022-03-01 15:04:12 +0530

    Update config.sample.js

--- config.sample.js
+++ config.sample.js
@@ -1,38 +1,38 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "XXXXXXXXXXX",
+	token: "OTQ4MTQ4OTc0Njg5MzQ5NjUy.Yh3miw.LLo8puiGhR4qtWQwHSAcDXoExE8",
 	/* For the support server */
 	support: {
-		id: "XXXXXXXXXXX", // The ID of the support server
-		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
+		id: "942374014737862706", // The ID of the support server
+		logs: "945244613118165003", // And the ID of the logs channel of your server (new servers for example)
 	},
 	/* Dashboard configuration */
 	dashboard: {
 		enabled: false, // whether the dashboard is enabled or not
-		secret: "XXXXXXXXXXX", // Your discord client secret
-		baseURL: "https://dashboard.atlanta-bot.fr", // The base URl of the dashboard
-		logs: "XXXXXXXXXXX", // The channel ID of logs
+		secret: "7ZD0V9bq7oACrFELrSharNniHehRgbAx", // Your discord client secret
+		baseURL: "http://freakbot.website2.me/", // The base URl of the dashboard
+		logs: "945244613118165003", // The channel ID of logs
 		port: 8080, // Dashboard port
 		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
-		failureURL: "https://www.atlanta-bot.fr" // url on which users will be redirected if they click the cancel button (discord authentication)
+		failureURL: "http://freakbot.website2.me/" // url on which users will be redirected if they click the cancel button (discord authentication)
 	},
-	mongoDB: "mongodb://localhost:27017/AtlantaBot", // The URl of the mongodb database
-	prefix: "*", // The default prefix for the bot
+	mongoDB: "mongodb+srv://hjop:hjop@cluster0.v9coj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", // The URl of the mongodb database
+	prefix: ".", // The default prefix for the bot
 	/* For the embeds (embeded messages) */
 	embed: {
 		color: "#0091fc", // The default color for the embeds
-		footer: "Atlanta | Open Source" // And the default footer for the embeds
+		footer: "Freak Development" // And the default footer for the embeds
 	},
 	/* Bot's owner informations */
 	owner: {
-		id: "XXXXXXXXXXX", // The ID of the bot's owner
-		name: "Androz#2091" // And the name of the bot's owner
+		id: "926889023954387038", // The ID of the bot's owner
+		name: "ᴅᴇsᴛɪᴎʏ♡тαиu#2900" // And the name of the bot's owner
 	},
 	/* DBL votes webhook (optional) */
 	votes: {
 		port: 5000, // The port for the server
 		password: "XXXXXXXXXXX", // The webhook auth that you have defined on discordbots.org
-		channel: "XXXXXXXXXXX" // The ID of the channel that in you want the votes logs
+		channel: "948150322772209724" // The ID of the channel that in you want the votes logs
 	},
 	/* The API keys that are required for certain commands */
 	apiKeys: {
@@ -51,17 +51,17 @@ module.exports = {
 	},
 	/* The others utils links */
 	others: {
-		github: "https://github.com/Androz2091", // Founder's github account
-		donate: "https://patreon.com/Androz2091" // Donate link
+		github: "", // Founder's github account
+		donate: "" // Donate link
 	},
 	/* The Bot status */
 	status: [
 		{
-			name: "@Atlanta help on {serversCount} servers",
+			name: "@Testing help on {serversCount} servers",
 			type: "LISTENING"
 		},
 		{
-			name: "my website : atlanta-bot.fr",
+			name: "my website : http://freakbot.website2.me/",
 			type: "PLAYING"
 		}
 	]
